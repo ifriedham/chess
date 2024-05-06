ba25ff5dc1f16e6c1b5c76b74dc0b073794ae401
ifriedham <noah10101@live.com>
2024-05-06 17:25:05 -0600

    bishop work done?

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -35,16 +35,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -55,15 +53,81 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        Collection<ChessMove> validMoves = new ArrayList<>();
+        Collection<ChessMove> moves = new ArrayList<>();
+        int[][] directions = getDirections(this.type);
+        int i = 0;
 
         switch (this.type) {
+            case KING -> {
+
+            }
+
+            case QUEEN -> {
+
+            }
+
+            case BISHOP -> {
+                for (int[] direction : directions) {
+                    moves.addAll(getLinearMoves(board, myPosition, direction[0], direction[1]));
+                }
+            }
+
+            case KNIGHT -> {
+
+            }
+
+            case ROOK -> {
+
+            }
+
+            case PAWN -> {
+
+            }
+        }
+
+        return moves;
+    }
+
+    private Collection<ChessMove> getLinearMoves(ChessBoard board, ChessPosition myPosition, int horizontalDir, int verticalDir) {
+        ArrayList<ChessMove> possibleMoves = new ArrayList<>();
+
+        for (int i = 1; i < 8; i++) {  // loops through all squares in a straight line (including diagonals?)
+            int nextRow = myPosition.getRow() + (i * horizontalDir);
+            int nextCol = myPosition.getColumn() + (i * verticalDir);
+
+            if (nextRow <= 0 || nextRow > 8 || nextCol <= 0 || nextCol > 8) break;  // next pos. invalid
+
+            ChessPiece nextSpot = board.getPiece(new ChessPosition(nextRow, nextCol));
+            if (nextSpot == null) {  // spot is empty, add to list
+                possibleMoves.add(new ChessMove(myPosition, new ChessPosition(nextRow, nextCol), null));
+            } else { // spot is occupied by an...
+                // ally
+                if (nextSpot.getTeamColor() == this.getTeamColor()) break;
+
+                    // enemy
+                else {
+                    possibleMoves.add(new ChessMove(myPosition, new ChessPosition(nextRow, nextCol), null));
+                    break;
+                }
+            }
+        }
+
+        return possibleMoves;
+    }
+
+    private int[][] getDirections(PieceType type) {
+        switch (type) {
             case KING -> {
             }
             case QUEEN -> {
             }
             case BISHOP -> {
-
+                return new int[][]{
+                        {1, 1},   // North East
+                        {-1, 1},  // South East
+                        {-1, -1}, // South West
+                        {1, -1}   // North West
+                };
             }
             case KNIGHT -> {
             }
@@ -73,6 +137,6 @@ public class ChessPiece {
             }
         }
 
-        return validMoves;
+        return new int[0][];
     }
 }
