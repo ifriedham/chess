5b031bbed3f2e2dc74418a9044b7fb39783a985b
ifriedham <noah10101@live.com>
2024-05-06 16:01:21 -0600

    main switch framework done

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -11,7 +11,12 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -30,6 +35,7 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
+
         throw new RuntimeException("Not implemented");
     }
 
@@ -37,6 +43,7 @@ public class ChessPiece {
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
+
         throw new RuntimeException("Not implemented");
     }
 
@@ -48,6 +55,24 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return new ArrayList<>();  // TEMP empty list returned here
+        Collection<ChessMove> validMoves = new ArrayList<>();
+
+        switch (this.type) {
+            case KING -> {
+            }
+            case QUEEN -> {
+            }
+            case BISHOP -> {
+
+            }
+            case KNIGHT -> {
+            }
+            case ROOK -> {
+            }
+            case PAWN -> {
+            }
+        }
+
+        return validMoves;
     }
 }
