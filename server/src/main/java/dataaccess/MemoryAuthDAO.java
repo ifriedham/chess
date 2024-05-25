2ea4879eff1398a49cfcccfc32414073dd5690d0
ifriedham <noah10101@live.com>
2024-05-25 16:49:01 -0600

    auth methods created

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,4 +1,29 @@
 package dataaccess;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public class MemoryAuthDAO implements AuthDAO {
+    private HashMap<String, String> auths;
+    public MemoryAuthDAO() {
+        auths = new HashMap<>();
+    }
+
+    public String createAuth(String username) {
+        String authToken = UUID.randomUUID().toString();
+        auths.put(username, authToken);
+        return authToken;
+    }
+
+    public String getAuth(String authToken) {
+        return auths.get(authToken);
+    }
+
+    public void deleteAuth(String authToken) {
+        auths.remove(authToken);
+    }
+
+    public void removeAllAuthTokens() {
+        auths.clear();
+    }
 }
