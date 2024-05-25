b03dd9c0a825bf9716ce819cb59698c5dd19df9f
ifriedham <noah10101@live.com>
2024-05-25 15:53:58 -0600

    AuthDAO methods support created

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,5 +1,8 @@
 package dataaccess;
 
 public interface AuthDAO {
-
+    String createAuth(String username) throws DataAccessException;
+    String getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+    void removeAllAuthTokens() throws DataAccessException;
 }
