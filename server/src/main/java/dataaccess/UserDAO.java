b249e2e30967a2e80d19d4f535d3e3abd5c91d98
ifriedham <noah10101@live.com>
2024-05-25 15:46:25 -0600

    userDAO method support created

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,5 +1,10 @@
 package dataaccess;
 
-public interface UserDAO {
+import model.UserData;
 
+public interface UserDAO {
+    UserData getUser(String username) throws DataAccessException;
+    //void createUser(String username, String password, String email) throws DataAccessException;
+    void createUser(UserData user) throws DataAccessException;
+    void removeAllUsers() throws DataAccessException;
 }
