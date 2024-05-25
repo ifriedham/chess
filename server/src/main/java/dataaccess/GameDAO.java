adaa8fc554d1a35ea91013a885243abbc1dcf2fe
ifriedham <noah10101@live.com>
2024-05-25 15:51:09 -0600

    gameDAO methods support created

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,5 +1,13 @@
 package dataaccess;
 
-public interface GameDAO {
+import model.GameData;
 
+import java.util.Collection;
+
+public interface GameDAO {
+    Collection<GameData> listGames() throws DataAccessException;
+    void createGame(String gameName) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    void SaveGame(int gameID, GameData game) throws DataAccessException;
+    void removeAllGames() throws DataAccessException;
 }
