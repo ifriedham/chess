15737cafcdac0130810af6bedf542191a552d905
ifriedham <noah10101@live.com>
2024-05-29 16:14:19 -0600

    gameService framework done

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,22 @@
+package service;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.HashMap;
+
+public class GameService {
+    public GameData createGame(AuthData authData, String gameName) {
+        return null;
+    }
+
+    public HashMap<Integer, GameData> listGames(AuthData authData) {
+        return null;
+    }
+
+    public GameData joinGame(AuthData authData, String playerColor, int gameID) {
+        return null;
+    }
+
+}
--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -8,9 +8,11 @@ public class UserService {
     public AuthData register(UserData user) {
         return null;
     }
+
     public AuthData login(UserData user) {
         return null;
     }
+
     public void logout(UserData user) {
     }
 
