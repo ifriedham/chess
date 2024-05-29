7e546ca72ce4f1ab142988999a37d39c74fe4c38
ifriedham <noah10101@live.com>
2024-05-29 16:38:53 -0600

    register done?

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,12 +1,35 @@
 package service;
 
 
+import dataaccess.*;
 import model.AuthData;
 import model.UserData;
 
 public class UserService {
-    public AuthData register(UserData user) {
-        return null;
+    private final AuthDAO authDAO;
+    private final UserDAO userDAO;
+
+    public UserService(AuthDAO authDAO, UserDAO userDAO) {
+        this.authDAO = authDAO;
+        this.userDAO = userDAO;
+    }
+
+
+    public AuthData register(UserData user) throws DataAccessException {
+         // test if all fields are filled
+         if (user.username() == null || user.password() == null || user.email() == null) {
+             throw new DataAccessException("bad request");
+         }
+
+        // test if username is already taken
+        if (userDAO.getUser(user.username()) != null) {
+            throw new DataAccessException("already taken");
+        }
+
+        userDAO.createUser(user);
+        String username = user.username();
+        String token = authDAO.createAuth(username);
+        return new AuthData(username, token);
     }
 
     public AuthData login(UserData user) {
