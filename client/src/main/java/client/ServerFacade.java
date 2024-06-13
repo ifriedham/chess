09231e98b95ded9453d6348e8416182c29544e7a
ifriedham <noah10101@live.com>
2024-06-13 15:09:04 -0600

    ServerFacade created

---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,13 @@
+package client;
+
+public class ServerFacade {
+    int port;
+
+    ServerFacade(int givenPort) {
+        port = givenPort;
+    }
+
+
+
+
+}
--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -7,12 +7,14 @@ import server.Server;
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(port);
     }
 
     @AfterAll
